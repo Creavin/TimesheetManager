25984aef47d7ce69a69230f686749ee1237d426e
Thomas Creavin <thomas.creavin@gmail.com>
2019-08-29 20:19:55 +0100

    wip: Saving times to file

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,12 +2,11 @@ import javafx.beans.property.SimpleStringProperty
 import javafx.scene.paint.Stop
 import tornadofx.*
 import tornadofx.Stylesheet.Companion.tab
-
+import java.io.File
 
 
 class Application : View("Timesheet Manager") {
     val name = SimpleStringProperty()
-
 
     override val root = borderpane{
         left = vbox {
@@ -16,10 +15,16 @@ class Application : View("Timesheet Manager") {
                 find<NewModalWindow>().openModal()
             }
 
-            button("Open Timesheet")
+            button("Write to file").setOnAction {
+                var newFile = File("tmp.text")
+                newFile.writeText("Hello there")
+
+            }
         }
         center = tabpane{
             tab<StopWatch>{
+
+
             }
 
             // Add tabs on demand when NewDocumentEvent is emitted
@@ -30,6 +35,10 @@ class Application : View("Timesheet Manager") {
                 }
             }
         }
+    }
+
+    init {
+        print(root.center.getToggleGroup())
     }
 
 
